17aabfa0dbb0f3361f43a1fb1b75a3857f5aad53
pawelrubin <pawelrubin19@gmail.com>
2018-05-11 00:27:45 +0200

    Interact mode. Window Layout is done.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,62 +1,81 @@
 package sample;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToolBar;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
+import java.beans.EventHandler;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Consumer;
 
 public class Controller {
+  public BorderPane borderPane;
+  public VBox vBox;
   public TextField mInput;
   public TextField nInput;
-  public Button startButton;
   public TextField pInput;
   public TextField msInput;
-  @FXML GridPane gridPane;
-  @FXML ToolBar toolBar;
-  @FXML VBox vBox;
+  public TextField sizeInput;
+  public Button startButton;
+  public ScrollPane scrollPane;
+  public GridPane gridPane;
+  
   private List<List<Thread>> threads = new ArrayList<>();
-
+  private Random random = Main.random;
+  private boolean check;
+  
+  // TODO zabijanie watkow
   @FXML public void startSimulation() {
     threads.clear();
     gridPane.getChildren().clear();
     Main.start = false;
-    int m = Integer.parseInt(mInput.getText());
-    int n = Integer.parseInt(nInput.getText());
-    double p = Double.parseDouble(pInput.getText());
-    long ms = Long.parseLong(msInput.getText());
-    // TODO szkalowanie
-    ColorSquare[][] colorSquares = new ColorSquare[m + 2][n + 2];
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        ColorSquare colorSquare = new ColorSquare(p, ms);
-        colorSquare.setHeight((gridPane.getHeight() - toolBar.getHeight())/m);
-        colorSquare.setWidth(gridPane.getWidth()/n);
+    int height = Integer.parseInt(mInput.getText());
+    int width = Integer.parseInt(nInput.getText());
+    double probability = Double.parseDouble(pInput.getText());
+    long delay = Long.parseLong(msInput.getText());
+    double size = Double.parseDouble(sizeInput.getText());
+  
+    ColorSquare[][] colorSquares = new ColorSquare[height + 2][width + 2];
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        ColorSquare colorSquare = new ColorSquare(probability, delay);
+        colorSquare.setHeight(size);
+        colorSquare.setWidth(size);
         colorSquares[i+1][j+1] = colorSquare;
-        gridPane.add(colorSquare, i, j);
+        gridPane.add(colorSquare,j,i);
       }
     }
-    for (int i = 1; i < m +1; i++) {
-      colorSquares[i][0] = colorSquares[i][n];
-      colorSquares[i][n +1] = colorSquares[i][1];
+    
+    // Setting somsiady
+    for (int i = 1; i < height + 1; i++) {
+      colorSquares[i][0] = colorSquares[i][width];
+      colorSquares[i][width +1] = colorSquares[i][1];
     }
     
-    for (int i = 1; i < n +1; i++) {
-      colorSquares[0][i] = colorSquares[m][i];
-      colorSquares[m +1][i] = colorSquares[1][i];
+    for (int i = 1; i < width + 1; i++) {
+      colorSquares[0][i] = colorSquares[height][i];
+      colorSquares[height +1][i] = colorSquares[1][i];
     }
-    colorSquares[0][0] = colorSquares[m][n];
-    colorSquares[0][n +1] = colorSquares[m][1];
-    colorSquares[m +1][0] = colorSquares[1][n];
-    colorSquares[m +1][n +1] = colorSquares[1][1];
+    colorSquares[0][0] = colorSquares[height][width];
+    colorSquares[0][width +1 ] = colorSquares[height][1];
+    colorSquares[height +1][0] = colorSquares[1][width];
+    colorSquares[height +1][width +1] = colorSquares[1][1];
     
-    for (int i = 1; i < m +1; i++) {
+    for (int i = 1; i < height + 1; i++) {
       List<Thread> threadsRow = new ArrayList<>();
-      for (int j = 1; j < n +1; j++) {
+      for (int j = 1; j < width + 1; j++) {
         ColorSquare[] somsiady = new ColorSquare[4];
         
           somsiady[0] = colorSquares[i][j - 1];
@@ -70,14 +89,34 @@ public class Controller {
       }
       threads.add(threadsRow);
     }
-
+    // end of setting somsiady xD
+    
     Main.start = true;
 
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
         threads.get(i).get(j).start();
       }
     }
     
+  }
+  
+  private void interact(MouseEvent mouseEvent) {
+      ColorSquare colorSquare = (ColorSquare) mouseEvent.getTarget();
+      System.out.println("changing color of this");
+      double red = random.nextDouble();
+      double green = random.nextDouble();
+      double blue = random.nextDouble();
+      Color color = Color.color(red, green, blue);
+      Platform.runLater(() -> colorSquare.setFill(color));
+  }
+  
+  public void interactButton(ActionEvent actionEvent) {
+    check ^= true;
+    if (check) {
+      gridPane.setOnMouseMoved(this::interact);
+    } else {
+      gridPane.setOnMouseMoved(null);
+    }
   }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Dyskoteka");
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
         primaryStage.setOnCloseRequest(event -> System.exit(0));

45e92a4fb69f9dd4d997afb5df818528bf931ae0
pawelostr <paewlostr@gmail.com>
2018-05-10 23:20:13 +0200

    walidacja danych + alert

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -21,63 +21,74 @@ public class Controller {
   @FXML VBox vBox;
   private List<List<Thread>> threads = new ArrayList<>();
 
-  @FXML public void startSimulation() {
-    threads.clear();
-    gridPane.getChildren().clear();
-    Main.start = false;
-    int m = Integer.parseInt(mInput.getText());
-    int n = Integer.parseInt(nInput.getText());
-    double p = Double.parseDouble(pInput.getText());
-    long ms = Long.parseLong(msInput.getText());
-    // TODO szkalowanie
-    ColorSquare[][] colorSquares = new ColorSquare[m + 2][n + 2];
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        ColorSquare colorSquare = new ColorSquare(p, ms);
-        colorSquare.setHeight((gridPane.getHeight() - toolBar.getHeight())/m);
-        colorSquare.setWidth(gridPane.getWidth()/n);
-        colorSquares[i+1][j+1] = colorSquare;
-        gridPane.add(colorSquare, i, j);
+  @FXML public void startSimulation() throws IllegalArgumentException {
+    try {
+      int m = Integer.parseInt(mInput.getText());
+      int n = Integer.parseInt(nInput.getText());
+      double p = Double.parseDouble(pInput.getText());
+      long ms = Long.parseLong(msInput.getText());
+
+      if (m < 1 || n < 1) {
+        Alert.display("Nieprawidłowe dane", "Błędne wymiary");
+        throw new IllegalArgumentException("Wrong width or height");
       }
-    }
-    for (int i = 1; i < m +1; i++) {
-      colorSquares[i][0] = colorSquares[i][n];
-      colorSquares[i][n +1] = colorSquares[i][1];
-    }
-    
-    for (int i = 1; i < n +1; i++) {
-      colorSquares[0][i] = colorSquares[m][i];
-      colorSquares[m +1][i] = colorSquares[1][i];
-    }
-    colorSquares[0][0] = colorSquares[m][n];
-    colorSquares[0][n +1] = colorSquares[m][1];
-    colorSquares[m +1][0] = colorSquares[1][n];
-    colorSquares[m +1][n +1] = colorSquares[1][1];
-    
-    for (int i = 1; i < m +1; i++) {
-      List<Thread> threadsRow = new ArrayList<>();
-      for (int j = 1; j < n +1; j++) {
-        ColorSquare[] somsiady = new ColorSquare[4];
-        
+
+      threads.clear();
+      gridPane.getChildren().clear();
+      Main.start = false;
+      // TODO szkalowanie
+      ColorSquare[][] colorSquares = new ColorSquare[m + 2][n + 2];
+      for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+          ColorSquare colorSquare = new ColorSquare(p, ms);
+          colorSquare.setHeight((gridPane.getHeight() - toolBar.getHeight()) / m);
+          colorSquare.setWidth(gridPane.getWidth() / n);
+          colorSquares[i + 1][j + 1] = colorSquare;
+          gridPane.add(colorSquare, i, j);
+        }
+      }
+      for (int i = 1; i < m + 1; i++) {
+        colorSquares[i][0] = colorSquares[i][n];
+        colorSquares[i][n + 1] = colorSquares[i][1];
+      }
+
+      for (int i = 1; i < n + 1; i++) {
+        colorSquares[0][i] = colorSquares[m][i];
+        colorSquares[m + 1][i] = colorSquares[1][i];
+      }
+      colorSquares[0][0] = colorSquares[m][n];
+      colorSquares[0][n + 1] = colorSquares[m][1];
+      colorSquares[m + 1][0] = colorSquares[1][n];
+      colorSquares[m + 1][n + 1] = colorSquares[1][1];
+
+      for (int i = 1; i < m + 1; i++) {
+        List<Thread> threadsRow = new ArrayList<>();
+        for (int j = 1; j < n + 1; j++) {
+          ColorSquare[] somsiady = new ColorSquare[4];
+
           somsiady[0] = colorSquares[i][j - 1];
           somsiady[1] = colorSquares[i][j + 1];
           somsiady[2] = colorSquares[i - 1][j];
           somsiady[3] = colorSquares[i + 1][j];
 
-        colorSquares[i][j].setSomsiady(somsiady);
-        Thread thread = new Thread(colorSquares[i][j]);
-        threadsRow.add(thread);
+          colorSquares[i][j].setSomsiady(somsiady);
+          Thread thread = new Thread(colorSquares[i][j]);
+          threadsRow.add(thread);
+        }
+        threads.add(threadsRow);
       }
-      threads.add(threadsRow);
-    }
 
-    Main.start = true;
+      Main.start = true;
 
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        threads.get(i).get(j).start();
+      for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+          threads.get(i).get(j).start();
+        }
       }
+
+    } catch (NumberFormatException e) {
+      Alert.display("Błędne dane","Proszę wprowadzić liczby");
+      e.printStackTrace();
     }
-    
   }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Symulacja");
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
         primaryStage.setOnCloseRequest(event -> System.exit(0));
